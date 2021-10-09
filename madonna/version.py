0d02585e299ceca166b23d8928a4e9465f9088ad
Tom Fleet <tomfleet2018@gmail.com>
2021-10-09 12:54:31 +0100

    Initial working implementation of less than

--- madonna/version.py
+++ madonna/version.py
@@ -1,19 +1,128 @@
 """
 Core semantic version functionality.
 
-
 Author: Tom Fleet
 Created: 08/10/2021
 """
 
+from __future__ import annotations
+
+import re
 from typing import Optional
 
-from pydantic import BaseModel
 
+class Version:
+    def __init__(
+        self,
+        major: int,
+        minor: int,
+        patch: int,
+        prerelease: Optional[str] = None,
+        buildmetadata: Optional[str] = None,
+    ) -> None:
+        """
+        A data container for a semantic version.
 
-class Version(BaseModel):
-    major: int
-    minor: int
-    patch: int
-    prerelease: Optional[str]
-    buildmetadata: Optional[str]
+        Args:
+            major (int): The major version, to be incremented when making
+                backwards incompatible (breaking) changes.
+            minor (int): The minor version, to be incremented when introducing
+                backwards compatible features.
+            patch (int): The patch version, to be incremented when making
+                backwards compatible bug fixes.
+            prerelease (Optional[str], optional): Any pre-release tags
+                e.g. 'rc.1'. Defaults to None.
+            buildmetadata (Optional[str], optional): Any build meta tags
+                e.g. 'build.123'. Defaults to None.
+
+        Raises:
+            ValueError: If any numeric version part < 0.
+        """
+        self.major = major
+        self.minor = minor
+        self.patch = patch
+        self.prerelease = prerelease
+        self.buildmetadata = buildmetadata
+
+        if any(part < 0 for part in (self.major, self.minor, self.patch)):
+            raise ValueError(
+                f"Version {self!r} is invalid. Parts cannot be less than 0."
+            )
+
+    def __repr__(self) -> str:
+        return (
+            self.__class__.__qualname__
+            + f"(major={self.major!r}, minor={self.minor!r}, patch={self.patch!r}, "
+            + f"prerelease={self.prerelease!r}, buildmetadata={self.buildmetadata!r})"
+        )
+
+    def __str__(self) -> str:
+        ver = f"v{self.major}.{self.minor}.{self.patch}"
+
+        if self.prerelease:
+            ver += f"-{self.prerelease}"
+
+        if self.buildmetadata:
+            ver += f"-{self.buildmetadata}"
+
+        return ver
+
+    def __lt__(self, v: Version) -> bool:
+        if not isinstance(v, Version):
+            raise NotImplementedError(
+                f"Cannot compare object of type Version and {type(v)}"
+            )
+
+        diff = (self.major, self.minor, self.patch) < (v.major, v.minor, v.patch)
+        if diff:
+            # If the numeric versions are less, return True straight away
+            return True
+
+        # If our version has a pre-release and the other doesn't
+        # ours is less
+        if self.prerelease and not v.prerelease:
+            return True
+
+        # If our version has a build tag and the other doesn't
+        # ours is less
+        if self.buildmetadata and not v.buildmetadata:
+            return True
+
+        # If our version has a prerelease of e.g. 'rc.1' and other
+        # has e.g. 'rc.2', ours is less
+        if self.prerelease and v.prerelease:
+            our_rc = re.match(pattern=r"rc.(?P<rc_ver>\d+)", string=self.prerelease)
+            other_rc = re.match(pattern=r"rc.(?P<rc_ver>\d+)", string=v.prerelease)
+
+            if not our_rc or not other_rc:
+
+                return False
+
+            our_rc_ver = int(our_rc.group("rc_ver"))
+            other_rc_ver = int(other_rc.group("rc_ver"))
+
+            if our_rc_ver < other_rc_ver:
+                return True
+
+        # If our version has a build of e.g. 'build.123' and other
+        # has e.g. 'build.456', ours is less
+        if self.buildmetadata and v.buildmetadata:
+            our_build = re.match(
+                pattern=r"build.(?P<build_ver>\d+)", string=self.buildmetadata
+            )
+            other_build = re.match(
+                pattern=r"build.(?P<build_ver>\d+)", string=v.buildmetadata
+            )
+
+            if not our_build or not other_build:
+
+                return False
+
+            our_build_ver = int(our_build.group("build_ver"))
+            other_build_ver = int(other_build.group("build_ver"))
+
+            if our_build_ver < other_build_ver:
+                return True
+
+        # We've exhausted every scenario we know how to compare
+        return False
---
+++ tests/test_version.py
@@ -0,0 +1,146 @@
+"""
+Tests for the core functionality.
+
+Author: Tom Fleet
+Created: 09/10/2021
+"""
+
+import pytest
+
+from madonna import Version
+
+
+def test_version_init():
+    v = Version(1, 2, 4, "rc.1", "build.123")
+    assert v.major == 1
+    assert v.minor == 2
+    assert v.patch == 4
+    assert v.prerelease == "rc.1"
+    assert v.buildmetadata == "build.123"
+
+
+def test_version_repr():
+    v = Version(1, 2, 4, "rc.1", "build.123")
+    want = "Version(major=1, minor=2, patch=4, prerelease='rc.1', buildmetadata='build.123')"  # noqa: E501
+    assert repr(v) == want
+
+
+def test_version_less_than_zero_major():
+    """
+    Initialising a version with any part < 0 should raise
+    a ValueError.
+    """
+    with pytest.raises(ValueError):
+        Version(-1, 2, 4)
+
+
+def test_version_less_than_zero_minor():
+    """
+    Initialising a version with any part < 0 should raise
+    a ValueError.
+    """
+    with pytest.raises(ValueError):
+        Version(1, -2, 4)
+
+
+def test_version_less_than_zero_patch():
+    """
+    Initialising a version with any part < 0 should raise
+    a ValueError.
+    """
+    with pytest.raises(ValueError):
+        Version(1, 2, -4)
+
+
+@pytest.mark.parametrize(
+    "version, want",
+    [
+        (Version(1, 2, 4, None, None), "v1.2.4"),
+        (Version(1, 2, 4), "v1.2.4"),
+        (Version(2, 6, 8, "rc.2", None), "v2.6.8-rc.2"),
+        (Version(2, 6, 8, "rc.2"), "v2.6.8-rc.2"),
+        (Version(7, 6, 2, None, "build.123"), "v7.6.2-build.123"),
+        (Version(7, 2, 1, "rc.1", "build.123"), "v7.2.1-rc.1-build.123"),
+    ],
+)
+def test_version_str(version: Version, want: str):
+    assert str(version) == want
+
+
+@pytest.mark.parametrize(
+    "v1, v2, want",
+    [
+        (Version(6, 4, 2), Version(6, 4, 3), True),  # Patch lt
+        (Version(6, 4, 3), Version(6, 4, 2), False),  # Patch gt
+        (Version(6, 4, 2), Version(6, 5, 2), True),  # Minor lt
+        (Version(6, 5, 2), Version(6, 4, 2), False),  # Minor gt
+        (Version(6, 4, 2), Version(7, 4, 2), True),  # Major lt
+        (Version(7, 4, 2), Version(6, 4, 2), False),  # Major gt
+        (Version(0, 0, 0), Version(9, 9, 9), True),  # Big diff true
+        (Version(9, 9, 9), Version(0, 0, 0), False),  # Big diff false
+        (Version(2, 2, 2), Version(2, 2, 2), False),  # Equal
+        (Version(0, 0, 0), Version(0, 0, 0), False),  # Zeros
+        (
+            Version(1, 2, 4),
+            Version(1, 2, 4, "rc.1"),
+            False,
+        ),
+        (
+            Version(1, 2, 4, "rc.1"),
+            Version(1, 2, 4),
+            True,
+        ),
+        (
+            Version(1, 2, 4),
+            Version(1, 2, 4, "rc.1", "build.123"),
+            False,
+        ),
+        (
+            Version(1, 2, 4, "rc.1", "build.123"),
+            Version(1, 2, 4),
+            True,
+        ),
+        (
+            Version(1, 2, 4, "rc.1"),
+            Version(1, 2, 4, "rc.2"),
+            True,
+        ),
+        (
+            Version(1, 2, 4, "rc.2"),
+            Version(1, 2, 4, "rc.1"),
+            False,
+        ),
+        (
+            Version(1, 2, 4, None, "build.123"),
+            Version(1, 2, 4, None, "build.456"),
+            True,
+        ),
+        (
+            Version(1, 2, 4, None, "build.456"),
+            Version(1, 2, 4, None, "build.123"),
+            False,
+        ),
+        (
+            Version(1, 2, 4, "rc.1", "build.123"),
+            Version(1, 2, 4, "rc.2", "build.123"),
+            True,
+        ),
+        (
+            Version(1, 2, 4, "rc.2", "build.123"),
+            Version(1, 2, 4, "rc.1", "build.123"),
+            False,
+        ),
+        (
+            Version(1, 2, 4, "rc.1", "build.456"),
+            Version(1, 2, 4, "rc.1", "build.123"),
+            False,
+        ),
+        (
+            Version(1, 2, 4, "rc.1", "build.123"),
+            Version(1, 2, 4, "rc.1", "build.456"),
+            True,
+        ),
+    ],
+)
+def test_version_less_than(v1: Version, v2: Version, want: bool):
+    assert (v1 < v2) == want
