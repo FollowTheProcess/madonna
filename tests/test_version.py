16cfb0c07c3a2f8960a3f536e35923e3d034f20f
Tom Fleet <tomfleet2018@gmail.com>
2021-10-10 20:18:37 +0100

    Add general comparison utility functions

--- tests/test_version.py
+++ tests/test_version.py
@@ -70,104 +70,76 @@ def test_version_str(version: Version, want: str):
 @pytest.mark.parametrize(
     "v1, v2, want",
     [
-        (Version(6, 4, 2), Version(6, 4, 3), True),  # Patch lt
-        (Version(6, 4, 3), Version(6, 4, 2), False),  # Patch gt
-        (Version(6, 4, 2), Version(6, 5, 2), True),  # Minor lt
-        (Version(6, 5, 2), Version(6, 4, 2), False),  # Minor gt
-        (Version(6, 4, 2), Version(7, 4, 2), True),  # Major lt
-        (Version(7, 4, 2), Version(6, 4, 2), False),  # Major gt
-        (Version(0, 0, 0), Version(9, 9, 9), True),  # Big diff true
-        (Version(9, 9, 9), Version(0, 0, 0), False),  # Big diff false
-        (Version(2, 2, 2), Version(2, 2, 2), False),  # Equal
-        (Version(0, 0, 0), Version(0, 0, 0), False),  # Zeros
-        (
-            Version(1, 2, 4),
-            Version(1, 2, 4, "rc.1"),
-            False,
-        ),
-        (
-            Version(1, 2, 4, "rc.1"),
-            Version(1, 2, 4),
-            True,
-        ),
-        (
-            Version(1, 2, 4),
-            Version(1, 2, 4, "rc.1", "build.123"),
-            False,
-        ),
-        (
-            Version(1, 2, 4, "rc.1", "build.123"),
-            Version(1, 2, 4),
-            True,
-        ),
-        (
-            Version(1, 2, 4, "rc.1"),
-            Version(1, 2, 4, "rc.2"),
-            True,
-        ),
-        (
-            Version(1, 2, 4, "rc.2"),
-            Version(1, 2, 4, "rc.1"),
-            False,
-        ),
-        (
-            Version(1, 2, 4, None, "build.123"),
-            Version(1, 2, 4, None, "build.456"),
-            True,
-        ),
-        (
-            Version(1, 2, 4, None, "build.456"),
-            Version(1, 2, 4, None, "build.123"),
-            False,
-        ),
-        (
-            Version(1, 2, 4, "rc.1", "build.123"),
-            Version(1, 2, 4, "rc.2", "build.123"),
-            True,
-        ),
-        (
-            Version(1, 2, 4, "rc.2", "build.123"),
-            Version(1, 2, 4, "rc.1", "build.123"),
-            False,
-        ),
-        (
-            Version(1, 2, 4, "rc.1", "build.456"),
-            Version(1, 2, 4, "rc.1", "build.123"),
-            False,
-        ),
-        (
-            Version(1, 2, 4, "rc.1", "build.123"),
-            Version(1, 2, 4, "rc.1", "build.456"),
-            True,
-        ),
-        (
-            Version(1, 2, 4, "rc.1", "build.123"),
-            Version(1, 2, 4, "rc.1"),
-            True,
-        ),
+        (Version(1, 0, 0, "equal"), Version(1, 0, 0, "equal"), 0),
+        (Version(1, 0, 0, "pre1"), Version(1, 0, 0, "hello1"), 0),
+        (Version(1, 0, 0, "pre"), Version(1, 0, 0), -1),
+        (Version(1, 0, 0), Version(1, 0, 0, "pre"), 1),
+        (Version(1, 0, 0, "pre1"), Version(1, 0, 0, "pre2"), -1),
+        (Version(1, 0, 0, "pre2"), Version(1, 0, 0, "pre1"), 1),
     ],
 )
-def test_version_less_than(v1: Version, v2: Version, want: bool):
-    assert (v1 < v2) == want
+def test_compare_prerelease(v1: Version, v2: Version, want: int):
+    assert v1._compare_prerelease(v2) == want
 
 
-def test_version_less_than_raises_if_invalid_object():
-    v1 = Version(1, 2, 4)
-    v2 = "I'm a string"
+def test_compare_prerelease_raises_if_no_digits():
+    v1 = Version(1, 0, 0, "pre")
+    v2 = Version(1, 0, 0, "prepre")
 
-    with pytest.raises(NotImplementedError):
-        v1 < v2
+    with pytest.raises(ValueError):
+        v1._compare_prerelease(v2)
+
+
+def test_compare_prerelease_raises_if_multiple_digits():
+    v1 = Version(1, 0, 0, "pre3")
+    v2 = Version(1, 0, 0, "pre3version4")
+
+    with pytest.raises(ValueError):
+        v1._compare_prerelease(v2)
+
+
+def test_compare_prerelease_raises_at_the_end():
+    v1 = Version(1, 0, 0, "cantcompareme")
+    v2 = Version(1, 0, 0, "orme")
+
+    with pytest.raises(ValueError):
+        v1._compare_prerelease(v2)
 
 
 @pytest.mark.parametrize(
-    "v1, v2",
+    "v1, v2, want",
     [
-        (Version(5, 2, 4, "badrelease1"), Version(5, 2, 4, "badrelease2")),
-        (Version(5, 2, 4, None, "badbuild1"), Version(5, 2, 4, None, "badbuild2")),
+        (Version(1, 0, 0, None, "equal"), Version(1, 0, 0, None, "equal"), 0),
+        (Version(1, 0, 0, None, "build1"), Version(1, 0, 0, None, "hello1"), 0),
+        (Version(1, 0, 0, None, "build"), Version(1, 0, 0), -1),
+        (Version(1, 0, 0), Version(1, 0, 0, None, "build"), 1),
+        (Version(1, 0, 0, None, "build1"), Version(1, 0, 0, None, "build2"), -1),
+        (Version(1, 0, 0, None, "build2"), Version(1, 0, 0, None, "build1"), 1),
     ],
 )
-def test_version_less_than_raises_if_cannot_parse_pre_or_build(
-    v1: Version, v2: Version
-):
+def test_compare_build(v1: Version, v2: Version, want: int):
+    assert v1._compare_build(v2) == want
+
+
+def test_compare_build_raises_if_no_digits():
+    v1 = Version(1, 0, 0, None, "build")
+    v2 = Version(1, 0, 0, None, "buildybuild")
+
     with pytest.raises(ValueError):
-        v1 < v2
+        v1._compare_build(v2)
+
+
+def test_compare_build_raises_if_multiple_digits():
+    v1 = Version(1, 0, 0, None, "build3")
+    v2 = Version(1, 0, 0, None, "build1ver4")
+
+    with pytest.raises(ValueError):
+        v1._compare_build(v2)
+
+
+def test_compare_build_raises_at_the_end():
+    v1 = Version(1, 0, 0, None, "cantcompareme")
+    v2 = Version(1, 0, 0, None, "orme")
+
+    with pytest.raises(ValueError):
+        v1._compare_build(v2)
